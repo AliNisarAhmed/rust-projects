791bd0c35e8af9781c6471c9acc81523f3959cbb
Ali Nisar Ahmed <ali.nisar87@gmail.com>
2022-05-10 21:40:59 -0600

    password gen improvements

--- 27-password-gen/src/main.rs
+++ 27-password-gen/src/main.rs
@@ -1,8 +1,17 @@
 use rand;
 use rand::seq::SliceRandom;
+use cli_clipboard::{ClipboardContext, ClipboardProvider};
+use std::io::{stdin, Read};
+
+const LETTERS: &[char] = &[
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+    't', 'u', 'v', 'w', 'x', 'y', 'z',
+];
+const SPECIAL_CHARS: &[char] = &['!', '@', '#', '$', '%', '^', '&', '*'];
+const NUMBERS: &[char] = &['1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
 fn main() {
-    let stdin = std::io::stdin();
+    let mut stdin = stdin();
 
     let mut min_len = String::new();
     let mut num_special_chars = String::new();
@@ -12,41 +21,49 @@ fn main() {
 
     stdin.read_line(&mut min_len).unwrap();
 
-    let min_len = min_len.trim().parse::<u32>().unwrap();
+    let min_len = min_len.trim().parse::<usize>().unwrap();
 
     println!("How many special characters?");
 
     stdin.read_line(&mut num_special_chars).unwrap();
 
-    let num_special_chars = num_special_chars.trim().parse::<u32>().unwrap();
+    let num_special_chars = num_special_chars.trim().parse::<usize>().unwrap();
 
     println!("How many numbers?");
 
     stdin.read_line(&mut num_numbers).unwrap();
 
-    let num_numbers = num_numbers.trim().parse::<u32>().unwrap();
+    let num_numbers = num_numbers.trim().parse::<usize>().unwrap();
 
-    let letters: &[char] = &[
-        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
-        's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-    ];
+    let password = generate_password(min_len, num_special_chars, num_numbers);
 
-    let special_chars: &[char] = &['!', '@', '#', '$', '%', '^', '&', '*'];
-    let numbers: &[char] = &['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+    // copy_password_to_clipboard(password);
 
+    let mut ctx = ClipboardContext::new().unwrap();
+    ctx.set_contents(password).unwrap();
+
+    println!("Your password has been copied to the clipboard!");
+
+    println!("Store your password somewhere safe!, then press any button to exit");
+
+    stdin.read(&mut [0]).unwrap();
+
+}
+
+fn generate_password(min_len: usize, num_special_chars: usize, num_numbers: usize) -> String {
     let mut rng = &mut rand::thread_rng();
 
-    let mut sp: Vec<_> = special_chars
+    let mut sp: Vec<_> = SPECIAL_CHARS
         .choose_multiple(&mut rng, num_special_chars as usize)
         .cloned()
         .collect();
 
-    let mut ns: Vec<char> = numbers
+    let mut ns: Vec<char> = NUMBERS
         .choose_multiple(&mut rng, num_numbers as usize)
         .cloned()
         .collect();
 
-    let mut ls: Vec<char> = letters
+    let mut ls: Vec<char> = LETTERS
         .choose_multiple(
             &mut rng,
             (min_len - num_numbers - num_special_chars) as usize,
@@ -59,5 +76,6 @@ fn main() {
 
     sp.shuffle(&mut rng);
 
-    println!("sample: {}", sp.into_iter().collect::<String>());
+    sp.into_iter().collect()
+
 }
